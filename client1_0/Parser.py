b15add02a83e978363eb26f95966c76edaae2e4b
zzh <1018634730@qq.com>
2024-07-17 23:28:37 +0800

    更改

---
+++ client1_0/Parser.py
@@ -0,0 +1,93 @@
+import re
+import pandas as pd
+from tqdm import tqdm
+
+para = {"template": "G:/item/deeplog/logparser/result/messages.log_templates.csv", "structured_file": "./test.csv"}
+
+
+# 读取日志条
+def data_log(log):
+    datas = []
+    lines = log
+    for line in tqdm(lines):
+        row = line.strip("\n").split()
+        t = [item.lstrip(" ") for item in row]
+        datas.append(t)
+    return datas
+
+
+def convert_template_to_regex(template):
+    template = template.replace('<*>', '.*')
+    # 再处理 **
+    template = template.replace('**', r'\*\*')
+    # 处理单个左括号或右括号的情况
+    template = template.replace('(', r'\(')
+    template = template.replace(')', r'\)')
+    template = template.replace('[', r'\[')
+    template = template.replace(']', r'\]')
+    return template
+
+
+# 读取模板并匹配
+def match(BGL):
+    template_df = pd.read_csv(para["template"])
+
+    event = []
+    event2id = {}
+
+    for i in range(template_df.shape[0]):
+        event_id = template_df.iloc[i, template_df.columns.get_loc("EventId")]
+        event_template = template_df.iloc[i, template_df.columns.get_loc("EventTemplate")]
+        # print(event_template)
+        regex_template = convert_template_to_regex(event_template)
+        # print(regex_template)
+        event2id[regex_template] = event_id
+        event.append(regex_template)
+
+    error_log = []
+    eventmap = []
+    print("Matching...")
+    for log in tqdm(BGL):
+        log_event = " ".join(log[5:]).lstrip(' ')
+        matched = False
+        for item in event:
+            # print(item)
+            if re.fullmatch(r'' + item, log_event):
+                eventmap.append(event2id[item])
+                matched = True
+                break
+        if not matched:
+            eventmap.append('error')
+            # print(eventmap)
+            error_log.append(log_event)
+    return eventmap
+
+
+def structure(BGL, eventmap):
+    month = []
+    day = []
+    time = []
+    for log in tqdm(BGL):
+        month.append(log[0])  # 提取时间戳
+        day.append(log[1])  # 提取时间戳
+        time.append(log[2])  # 提取时间戳
+
+    BGL_structured = pd.DataFrame(columns=["Month", "Day", "Time", "EventId"])
+    BGL_structured["Month"] = month
+    BGL_structured["Day"] = day
+    BGL_structured["Time"] = time
+    BGL_structured["EventId"] = eventmap
+    # Remove logs which do not match the template
+    # BGL_structured = BGL_structured[BGL_structured["EventId"] != "error"]
+    BGL_structured.to_csv(para["structured_file"], index=None)
+
+
+def process_data(log):
+    logs = [
+        log
+    ]
+    BGL = data_log(logs)
+    eventId = match(BGL)
+    print(eventId)
+    structure(BGL, eventId)
+    return int(re.findall(r'\d+', eventId[0])[0])
---
+++ client1_0/Predicter.py
@@ -0,0 +1,51 @@
+import torch
+from collections import Counter
+
+
+class Predicter():
+    def __init__(self, model, options):
+        self.device = options['device']
+        self.model = model
+        self.model_path = options['model_path']
+        self.window_size = options['window_size']
+        self.num_candidates = options['num_candidates']
+        self.num_classes = options['num_classes']
+        self.input_size = options['input_size']
+        self.test_data = options['test_data']
+
+        # Load the model
+        self.model.load_state_dict(torch.load(self.model_path)['state_dict'])
+        self.model.to(self.device)
+        self.model.eval()
+
+    def process_data(self, data):
+        window_size = self.window_size
+        hdfs = {}
+        for ln in data:
+            ln = list(map(lambda n: n - 1, map(int, ln.strip().split())))
+            ln = ln + [-1] * (window_size + 1 - len(ln))
+            hdfs[tuple(ln)] = hdfs.get(tuple(ln), 0) + 1
+        return hdfs
+
+    def predict(self):
+        test_data_processed = self.process_data(self.test_data)
+
+        for line in test_data_processed.keys():
+            for i in range(len(line) - self.window_size):
+                seq0 = line[i:i + self.window_size]
+                label = line[i + self.window_size]
+                seq1 = [0] * self.num_classes
+                log_counter = Counter(seq0)
+                for key in log_counter:
+                    seq1[key] = log_counter[key]
+
+                seq0 = torch.tensor(seq0, dtype=torch.float).view(
+                    -1, self.window_size, self.input_size).to(self.device)
+                seq1 = torch.tensor(seq1, dtype=torch.float).view(
+                    -1, self.num_classes, self.input_size).to(self.device)
+                label = torch.tensor(label).view(-1).to(self.device)
+                output = self.model(features=[seq0, seq1], device=self.device)
+                predicted = torch.argsort(output, 1)[0][-self.num_candidates:]
+                if label not in predicted:
+                    return "异常"
+        return "正常"
